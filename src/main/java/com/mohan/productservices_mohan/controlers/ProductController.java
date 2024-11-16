32df1ca1bf2bc3eed30fce4bbc0762b17564fbdd
mohan <mohan.baskaran1998@gmail.com>
2024-11-16 19:05:38 +0530

    created product contrl cls

---
+++ src/main/java/com/mohan/productservices_mohan/controlers/ProductController.java
@@ -0,0 +1,14 @@
+package com.mohan.productservices_mohan.controlers;
+
+import com.mohan.productservices_mohan.modules.Product;
+
+public class ProductController {
+
+    public void getAllProducts() {}
+
+    public void getProductDetails(Long id) {}
+
+    public void createProduct() {}
+
+
+}
