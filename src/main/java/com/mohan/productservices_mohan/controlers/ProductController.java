036a464e3eaade8d9860dc90100d04fd8c479836
mohan <mohan.baskaran1998@gmail.com>
2024-11-16 19:31:33 +0530

    added some methods

--- src/main/java/com/mohan/productservices_mohan/controlers/ProductController.java
+++ src/main/java/com/mohan/productservices_mohan/controlers/ProductController.java
@@ -1,10 +1,17 @@
 package com.mohan.productservices_mohan.controlers;
 
 import com.mohan.productservices_mohan.modules.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ProductController {
 
-    public void getAllProducts() {}
+    @GetMapping(name = "/getAllProducts")
+    public void getAllProducts() {
+
+    }
 
     public void getProductDetails(Long id) {}
 
